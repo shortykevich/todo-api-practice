8925a178eb61ccbd498e59fa28930af798550bb5
Kirill Dvoretsky <shortykofficial@gmail.com>
2024-09-15 02:03:19 +0300

    Added base CRUD operations

--- todo_app/models.py
+++ todo_app/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String, Boolean
 from todo_app.database import Base
 
 
-class Todos(Base):
+class Todo(Base):
     __tablename__ = 'todos'
 
     id = Column(Integer, primary_key=True, index=True)
